81664b6f467ba0540ff5f7a129834c5be0436447
sananeo0 <sananeo@code.edu.az>
2023-10-29 23:02:59 +0400

    main

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ menuIcon.addEventListener("click", () => {
 
     options.style.transform = "translateX(18rem)";
     rightSide.style.display = "block"
-    rightSide.style.opacity = "1";
 
 });
 
 
 document.addEventListener("click", (event) => {
     if (!menuIcon.contains(event.target) && !options.contains(event.target)) {
-        rightSide.style.opacity = "0";
         rightSide.style.display = "none"
         options.style.transform = "translateX(-18rem)";
     }
